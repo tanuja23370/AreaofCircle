d96495fa05093eccb94bf3564717b677ee4aa3bc
tanuja23370 <tanujanimbakkanavar23@gmail.com>
2025-05-27 11:48:30 -0300

    save

---
+++ RectanglePerimeter.java
@@ -0,0 +1,25 @@
+package AreaOfCircle;
+
+public class RectanglePerimeter {
+    private double length;
+    private double breadth;
+
+    public RectanglePerimeter(double length, double breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
+
+    public double getArea() {
+        return length * breadth;
+    }
+
+    public double getPerimeter() {
+        return 2 * (length + breadth);
+    }
+
+    public static void main(String[] args) {
+        RectanglePerimeter rect = new RectanglePerimeter(10, 5);
+        System.out.println("Area of Rectangle: " + rect.getArea());
+        System.out.println("Perimeter of Rectangle: " + rect.getPerimeter());
+    }
+}
