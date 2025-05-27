74f8b855ed5579d3a27f49bc843d1e4a32ca63ae
tanuja23370 <tanujanimbakkanavar23@gmail.com>
2025-05-27 11:46:35 -0300

    save

---
+++ CircleArea.java
@@ -0,0 +1,23 @@
+package AreaOfCircle;
+
+public class CircleArea {
+    private double radius;
+
+    public CircleArea(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    public static void main(String[] args) {
+        CircleArea circle = new CircleArea(7);
+        System.out.println("Area of Circle: " + circle.getArea());
+        System.out.println("Perimeter of Circle: " + circle.getPerimeter());
+    }
+}
